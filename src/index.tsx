775cd3e7bf94617ad7e8e96e13b86fc99204ebc5
chanooda <hanrhfqkq@gmail.com>
2022-07-10 16:01:59 +0900

    feat : learn store and reducer
    createStore을 이용해 store을 생성하여 count를 관리
    데이터를 수정하는 Modifier 함수를 이용해 count의 숫자를 제어
    createStore(Modifier)
    store.subscribe로 현재값을 구독
    store.dispatch 함수 이용 객체를 인자로 전달하여 정보를 넘김

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,26 @@
+import { Reducer, ReducerAction, ReducerState } from "react";
+import { createStore } from "redux";
+
 const add = document.getElementById("add") as HTMLButtonElement;
 const minus = document.getElementById("minus") as HTMLButtonElement;
 const number = document.querySelector("span") as HTMLSpanElement;
 
-let count = 0;
-number.innerText = String(count);
+number.innerText = "0";
 
-const updateText = () => {
-  number.innerText = String(count);
+// reducer = data를 수정하는 함수
+const countModifier = (count = 0, action: any) => {
+  if (action.type === "ADD") return count + 1;
+  else if (action.type === "MINUS") return count - 1;
+  else return count;
 };
 
-const handleAdd = () => {
-  count = count + 1;
-  updateText();
+const countStore = createStore(countModifier);
+
+const onChange = () => {
+  number.innerText = String(countStore.getState());
 };
 
-const handleMinus = () => {
-  count = count - 1;
-  updateText();
-};
+countStore.subscribe(onChange);
 
-add.addEventListener("click", handleAdd);
-minus.addEventListener("click", handleMinus);
+add.addEventListener("click", () => countStore.dispatch({ type: "ADD" }));
+minus.addEventListener("click", () => countStore.dispatch({ type: "MINUS" }));
